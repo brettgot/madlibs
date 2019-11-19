97057877f68daa5a8d2fb89b57951d4770788c56
Brett Gottfried and Eric Halliday <eric@>
2019-11-18 20:43:13 -0800

    Try to make a scam website to steal credit card info

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
-$(function() {
+$(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val()
-    var person2Input = $("input#person2").val()
-    var animalInput = $("input#animal").val()
-    var exclamationInput = $("input#exclamation").val()
-    var verbInput = $("input#verb").val()
-    var nounInput = $("input#noun").val()
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    var customernameI = $("input#customername").val();
+    var customernamelastI = $("input#customernamelast").val();
+    var customeraddressI = $("input#customeraddress").val();
+    var customerstateI = $("input#customerstate").val();
+    var customerzipI = $("input#customerzip").val();
+    var creditcardI = $("input#creditcard").val();
 
-    $("#story").show();
 
+    $(".customername").text(customernameI);
+    $(".customernamelast").text(customernamelastI);
+    $(".customeraddress").text(customeraddressI);
+    $(".customerstate").text(customerstateI);
+    $(".customerzip").text(customerzipI);
+    $(".creditcard").text(creditcardI);
+
+
+    $("#recipt").show();
+    $("#blanks").hide();
+    $("#pagetitle").hide();
     event.preventDefault();
+
   });
 });
